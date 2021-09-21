3315f95b55cb43db8c51730b39652bb385e112c4
booklover4177 <anne.straub@gmail.com>
2021-09-21 13:18:25 -0400

    wrote withdrawl and deposit functions for Checking

--- src/Checking.cs
+++ src/Checking.cs
@@ -4,7 +4,43 @@ namespace CBank
 {
     public class Checking : User, IAccount{
         
-        
+        public double Balance;
+
+       hasChecking=true;
+
+        public bool IntialDeposit =true;
+        public double deposit(double depositAmount){
+
+            Balance+=depositAmount;
+
+            if(IntialDeposit==true){
+                userLogs.Add($"{name}, date, Initial Deposit, ${depositAmount}, Success, {Balance}");
+                IntialDeposit=false;
+                return Balance;
+            }else{
+
+                userLogs.Add($"{name}, date, Deposit, ${depositAmount}, Success, {Balance}");
+                return Balance;
+            }
+
+        }
+
+        public double withdraw(double withdrawalAmount){
+
+            if(Balance>=withdrawalAmount){
+
+                Balance-=withdrawalAmount;
+                 userLogs.Add($"{name}, date, Withdrawl, ${withdrawalAmount}, Success, {Balance}");
+                 return Balance;
+
+            }else{
+
+                userLogs.Add($"{name}, date, Withdrawl, ${withdrawalAmount}, Failure, {Balance}");
+                Balance-=50.00;
+                userLogs.Add($"{name}, date, Fee, $50, Success, {Balance}");
+                return Balance;
+            }
+        }
 
     }
 }
--- src/User.cs
+++ src/User.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace CBank
 {
     public class User : Bank{
       public string name;
-      public string[] userLogs;
-      public boolean hasChecking;
-      public boolean hasSavings;
+      public List<string> userLogs= new List<string>();
+      public bool hasChecking=false;
+      public bool hasSavings=false;
     }
 }
